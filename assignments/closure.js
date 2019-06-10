e32b2bd4aa2f8d741c4be231d8aa11874ff9a595
Seth Lewis <lewiscatenary@gmail.com>
2019-06-10 13:20:53 -0500

    main challenges & some stretch goals complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,45 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+let varying = 2;
+const returnVarying3 = () => {
+  return varying *= 3;
+}
+console.log(returnVarying3());
+console.log(returnVarying3());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
+let tally = 0;
 const counter = () => {
+  const ticker = () => {
+    return tally += 1;
+  }
+  return ticker();
   // Return a function that when invoked increments and returns a counter variable.
 };
+console.log(counter());
+console.log(counter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+// const counterFactory = () => {
+//   let counter = 0;
+//   const obj = {
+//     increment: function () {
+//       return counter += 1;
+//     },
+//     decrement: function () {
+//       return counter -= 1;
+//     }
+//   }
+//   return obj;
+//   // Return an object that has two methods called `increment` and `decrement`.
+//   // `increment` should increment a counter variable in closure scope and return it.
+//   // `decrement` should decrement the counter variable and return it.
+// };
+// counterFactory();
+// console.log(obj.increment());
