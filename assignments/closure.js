d8e23359c9f89aedd470e3a7122040cac49c7b63
Seth Lewis <lewiscatenary@gmail.com>
2019-06-11 11:30:27 -0500

    completed last task in closure. all challenges complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -21,25 +21,28 @@ const counter = () => {
 };
 console.log(counter());
 console.log(counter());
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-// const counterFactory = () => {
-//   let counter = 0;
-//   const obj = {
-//     increment: function () {
-//       return counter += 1;
-//     },
-//     decrement: function () {
-//       return counter -= 1;
-//     }
-//   }
-//   return obj;
-//   // Return an object that has two methods called `increment` and `decrement`.
-//   // `increment` should increment a counter variable in closure scope and return it.
-//   // `decrement` should decrement the counter variable and return it.
-// };
-// counterFactory();
-// console.log(obj.increment());
+// const count = 0;
+const counterFactory = (name) => {
+  function CountThing(name) {
+    this.name = name;
+    this.count = 0;
+    this.increment = function () {
+      return this.count += 1;
+    }
+    this.decrement = function () {
+      return this.count -= 1;
+    }
+  }
+  return new CountThing(name);
+  // Return an object that has two methods called `increment` and `decrement`.
+  // `increment` should increment a counter variable in closure scope and return it.
+  // `decrement` should decrement the counter variable and return it.
+};
+const robot = counterFactory("robot");
+console.log(`Counter Factory: ${ robot.name }`);
+console.log(`Counter Factory: ${ robot.count }`);
+console.log(`Counter Factory: ${ robot.increment() }`);
+console.log(`Counter Factory: ${ robot.decrement() }`);
